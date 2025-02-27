05b8529f841249aa81605fa546ca3f04906b2e28
arkajyoti-roy <arkajyotiroy445@gmail.com>
2025-02-27 21:38:06 +0530

    update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,10 +5,11 @@ import upload from '../utils/multer.js'; // Correct import path
 
 const router = express.Router();
 
-router.route('/signup').post(signup);
-router.route('/login').post(login);
-router.route('/profile').get(isAuthenticated, getUserProfile);
-router.route('/profile/update').put(isAuthenticated, upload.single('profilePhoto'), updateUserProfile); // Apply multer middleware
-router.route('/logout').post(logout);
+// Define routes
+router.route('/signup').post(signup); // Signup route
+router.route('/login').post(login); // Login route
+router.route('/profile').get(isAuthenticated, getUserProfile); // Get user profile route
+router.route('/profile/update').put(isAuthenticated, upload.single('profilePhoto'), updateUserProfile); // Update user profile route with multer middleware
+router.route('/logout').post(logout); // Logout route
 
 export default router;
